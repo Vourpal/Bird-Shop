49af4ff25658595a0b90e8c01d1ebfe137a0c2c3
Bruno Oros <hollowxdstorm@gmail.com>
2023-04-06 13:50:47 -0400

    initialized NavBar that will be used for majority of layouts

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,29 @@
+import { NavLink } from "react-router-dom";
+import "../Stylesheets/NavBar.css";
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>
-        This will be the Navbar found on all pages besides probably cart page
-      </h1>
+    <div id="navBar-wrapper">
+      <h1 id="store-name">Chirp</h1>
+      <nav className="store-links-wrapper">
+        <NavLink className="store-links" to="/pets">
+          Pets
+        </NavLink>
+        <NavLink className="store-links" to="/cages">
+          Cages
+        </NavLink>
+        <NavLink className="store-links" to="/food">
+          Food
+        </NavLink>
+        <NavLink className="store-links" to="/toys">
+          Toys
+        </NavLink>
+        <NavLink className="store-links" to="/cart">
+          Cart
+        </NavLink>
+      </nav>
     </div>
   );
 };
 
-export default NavBar
+export default NavBar;
