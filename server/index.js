3005ef4787ba54bd9def62dfa090aaa7429739a8
Bruno Oros <hollowxdstorm@gmail.com>
2023-04-18 16:16:20 -0400

    completed backend communication to frontend through a proxy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
-const {Item} = require("./db/schema")
+const { Item } = require("./db/schema");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var cors = require('cors')
 
 main().catch((err) => console.log(err));
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/webShop");
@@ -11,11 +18,29 @@ async function main() {
 }
 
 app.get("/food", async (req, res) => {
-  const food = await Item.find({category: "food"})
-  console.log(food)
-  res.send("ok it worked maybe xD?")
-})
+  const food = await Item.find({ category: "food" });
+  console.log(food);
+  res.send(food);
+});
 
+app.get("/cage", async (req, res) => {
+  const cage = await Item.find({ category: "cage" });
+  console.log(cage);
+  res.send(cage);
+});
+
+app.get("/bird", async (req, res) => {
+  const bird = await Item.find({ category: "bird" });
+  console.log(bird);
+  res.send(bird);
+});
+
+app.get("/home", (req, res) => {
+  res.json({
+    name: "Bill",
+    age: 99
+  });
+});
 app.listen(3000, () => {
   console.log("Server side port is on!");
 });
