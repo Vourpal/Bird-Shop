104f57d62408b01c77bada7578a565cc1bf8dfd0
Bruno Oros <hollowxdstorm@gmail.com>
2023-05-15 21:11:38 -0400

    formatted changes for itemdetails and cartLayout

--- client/src/Layout/CartLayout.jsx
+++ client/src/Layout/CartLayout.jsx
@@ -4,37 +4,45 @@ import Contact from "../Components/Contact";
 import { cartDataContext } from "./RootLayout";
 import { useContext } from "react";
 import { cartDataDispatchContext } from "./RootLayout";
+import "../Stylesheets/CartLayout.css";
 const CartLayout = () => {
   const tasks = useContext(cartDataContext);
   const dispatch = useContext(cartDataDispatchContext);
   return (
     <div>
       <NavBar />
-      {tasks.length === 0 ? (
-        <div>Your shopping cart is currently empty</div>
-      ) : (
-        tasks.map((data) => {
-          return (
-            <div className="cart-item-wrapper" key={data.name}>
-              <h1>{data.count}</h1>
-              <div>{data.details.name}</div>
-              <div>{data.details.price * data.count}</div>
-              <button onClick={() => {
-                dispatch({
-                  type: "deleted",
-                  id: data.details._id,
-                });
-              }}>Remove</button>
-            </div>
-          );
-        })
-      )}
-      <Contact />
+      <div id="cart-wrapper">
+        {tasks.length === 0 ? (
+          <h2 id="cart-empty">Your shopping cart is currently empty</h2>
+        ) : (
+          tasks.map((data) => {
+            return (
+              <div className="cart-item-wrapper" key={data.name}>
+                <h1>{data.count}</h1>
+                <div>{data.details.name}</div>
+                <div>{data.details.price * data.count}</div>
+                <button
+                  onClick={() => {
+                    dispatch({
+                      type: "deleted",
+                      id: data.details._id,
+                    });
+                  }}
+                >
+                  Remove
+                </button>
+              </div>
+            );
+          })
+        )}
+      </div>
+
+      <div id="contact-wrapper">
+        <Contact />
+      </div>
     </div>
   );
 };
 export default CartLayout;
 
-
-
-//  <img src={data.details.image}></img> 
+//  <img src={data.details.image}></img>
